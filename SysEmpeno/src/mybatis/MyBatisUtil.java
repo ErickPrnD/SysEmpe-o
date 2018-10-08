6fb00ef027daaed373b34428e61df17696709522
erick <erick@DESKTOP-UOELTU5>
2018-10-08 13:58:09 -0500

    Solo necesario

--- SysEmpeno/src/mybatis/MyBatisUtil.java
+++ SysEmpeno/src/mybatis/MyBatisUtil.java
@@ -1,11 +1,4 @@
 package mybatis;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package mybatis;
 import java.io.IOException;
 import java.io.Reader;
 import org.apache.ibatis.io.Resources;
