8dd5bbce3037bf42d52dec955068b3c6d949ab54
erick <erick@DESKTOP-UOELTU5>
2018-10-31 16:51:25 -0600

    Construccion interfaces

--- SysEmpeno/src/sysempeno/PruebasErick.java
+++ SysEmpeno/src/sysempeno/PruebasErick.java
@@ -9,6 +9,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -23,7 +26,23 @@ public class PruebasErick extends Application {
         
         Scene scene = new Scene(root);
         this.stage = stage;
+         MenuBar menuBar = new MenuBar();
+ 
+        // --- Menu File
+        Menu menuFile = new Menu("File");
+ 
+        // --- Menu Edit
+        Menu menuEdit = new Menu("Edit");
+ 
+        // --- Menu View
+        Menu menuView = new Menu("View");
+ 
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+ 
+ 
+        ((VBox) scene.getRoot()).getChildren().addAll(menuBar);
         stage.setScene(scene);
+        
         stage.show();
     }
 
